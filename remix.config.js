fc1014cd32c8fb09ee3f709181029291b76ded13
Juan D. Jara <juanorigami@gmail.com>
2022-10-05 17:01:14 +0200

    accesible emoji working

--- remix.config.js
+++ remix.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   mdx: async (filename) => {
     const [ariaEmojis, emojis, gfm, images] = await Promise.all([
-      import("rehype-accessible-emojis").then((mod) => mod.default),
+      import("rehype-accessible-emojis").then((mod) => mod.rehypeAccessibleEmojis),
       import('remark-gfm').then(mod => mod.default),
       import('remark-emoji').then(mod => mod.default),
       import('remark-images').then(mod => mod.default),
