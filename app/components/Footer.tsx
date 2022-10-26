04cb7df5deebd9df0600807cfe0c8a791a3892a0
Juan D. Jara <juanorigami@gmail.com>
2022-10-26 19:34:05 +0200

    Update file app/components/Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="my-12 flex text-sm items-center">
       <p>2022 © Juan D. Jara</p>
       <div className="flex-grow"></div>
-      <a className="ml-3 underline" href="https://pressunto.fly.dev/r/juandjara/remix-blog">Edit</a>
+      <a className="ml-3 underline" href="https://pressunto.fly.dev/projects/juandjara/remix-blog">Edit</a>
       <a className="ml-3 underline" href="/feed.xml">RSS</a>
     </footer>
   ) 
