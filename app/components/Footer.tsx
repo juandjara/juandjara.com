92fd5f9cca6dd09fe3ae893e80203a47a91b13bd
Juan D. Jara <juanorigami@gmail.com>
2022-11-15 14:40:10 +0100

    Update file app/components/Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="my-12 flex text-sm items-center">
-      <p>2022 © Juan D. Jara</p>
+      <p>Desarrollado con <span role="img" aria-label="red heart">❤️</span> por Juan D. Jara en 2022</p>
       <div className="flex-grow"></div>
       <a className="ml-3 underline" href="https://pressunto.fly.dev/projects/juandjara/remix-blog">Edit</a>
       <a className="ml-3 underline" href="/feed.xml">RSS</a>
