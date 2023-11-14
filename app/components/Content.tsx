46f1347482d4aa361360f49731fda2421230eff9
Juan D. Jara <juanorigami@gmail.com>
2023-11-14 16:00:56 +0100

    make list bullets white in dark mode

--- app/components/Content.tsx
+++ app/components/Content.tsx
@@ -1,6 +1,7 @@
 export default function Content({ children, padding = 'py-8 md:py-12' }: { children: React.ReactNode; padding?: string }) {
   const style = [
     'prose prose-orange prose-hr:border-gray-300 lg:prose-xl',
+    'prose-li:dark:marker:text-white',
     'dark:prose-invert',
     padding
   ].join(' ')
